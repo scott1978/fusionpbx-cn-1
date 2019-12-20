73ed7141a059906174a5d414f3edfb03c41b8211
hzxgo <hezhixiongbei@126.com>
2019-12-20 10:09:36 +0800

    landing route

---
+++ app/landing_route/app_menu.php
@@ -0,0 +1,30 @@
+<?php
+
+	$y=0;
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['de-at'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['de-ch'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['de-de'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['es-cl'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['fr-ca'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['it-it'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['sv-se'] = "Landing Route";
+	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Landing Route";
+	$apps[$x]['menu'][$y]['uuid'] = "3876ac58-7b37-49d2-8c34-9cf443b1b613";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "b94e8bd9-9eb5-e427-9c26-ff7a6c21552a";
+	$apps[$x]['menu'][$y]['category'] = "internal";
+	$apps[$x]['menu'][$y]['path'] = "/app/landing_route/route.php";
+	$apps[$x]['menu'][$y]['groups'][] = "admin";
+	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+
+?>
