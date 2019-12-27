892328b5a5a012783db2c7c3e67db8fb6db85ad0
hzxgo <hezhixiongbei@126.com>
2019-12-27 11:58:10 +0800

    test

--- app/landing_route/route_edit.php
+++ app/landing_route/route_edit.php
@@ -843,11 +843,11 @@
 // 	echo "	}\n";
 // 	echo "</script>\n";
 
-	echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n";
-	echo "<script src=\"".PROJECT_PATH."/resources/jquery/jquery-1.11.1.js\"></script>\n";
-	echo "<script src=\"".PROJECT_PATH."/resources/jquery/ztree/jquery.ztree.core.min.js\"></script>\n";
-	echo "<script src=\"".PROJECT_PATH."/resources/jquery/ztree/jquery.ztree.excheck.min.js\"></script>\n";
-	echo "<link rel=\"stylesheet\" href=\"".PROJECT_PATH."/resources/bootstrap/css/zTreeStyle/zTreeStyle.css\" />\n";
+	// echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\n";
+	// echo "<script src=\"".PROJECT_PATH."/resources/jquery/jquery-1.11.1.js\"></script>\n";
+	// echo "<script src=\"".PROJECT_PATH."/resources/jquery/ztree/jquery.ztree.core.min.js\"></script>\n";
+	// echo "<script src=\"".PROJECT_PATH."/resources/jquery/ztree/jquery.ztree.excheck.min.js\"></script>\n";
+	// echo "<link rel=\"stylesheet\" href=\"".PROJECT_PATH."/resources/bootstrap/css/zTreeStyle/zTreeStyle.css\" />\n";
 	echo "<script type='text/javascript'>\n";
 	echo "	var setting = { check: { enable: true }, data: { simpleData: { enable: true } } };\n";
 	echo "	var zNodes = [{id:1,pId:0,name:\"全国\",checked:true},{id:43,pId:1,name:\"湖南\",checked:true},{id:4301,pId:43,name:\"长沙\",checked:true},{id:4302,pId:43,name:\"株洲\",checked:true},{id:4303,pId:43,name:\"湘潭\",checked:true},{id:4304,pId:43,name:\"衡阳\",checked:true},{id:4305,pId:43,name:\"岳阳\",checked:true},{id:42,pId:1,name:\"湖北\",open:false},{id:4201,pId:42,name:\"武汉\"},{id:4302,pId:42,name:\"咸宁\"},];\n";
