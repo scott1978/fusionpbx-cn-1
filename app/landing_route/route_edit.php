3e61a34146144f04e60857927807d8196a4af6c1
hzxgo <hezhixiongbei@126.com>
2019-12-28 11:29:54 +0800

    test

--- app/landing_route/route_edit.php
+++ app/landing_route/route_edit.php
@@ -51,20 +51,6 @@
 		$action = "add";
 	}
 
-//set ztree nodes
-	// $zNodes = [
-	// 		{ id:1, pId:0, name:'全国', checked:true},
-	// 		{ id:43, pId:1, name:'湖南', checked:true},
-	// 		{ id:4301, pId:43, name:'长沙', checked:true},
-	// 		{ id:4302, pId:43, name:'株洲', checked:true},
-	// 		{ id:4303, pId:43, name:'湘潭', checked:true},
-	// 		{ id:4304, pId:43, name:'衡阳', checked:true},
-	// 		{ id:4305, pId:43, name:'岳阳', checked:true},
-	// 		{ id:42, pId:1, name:'湖北', open:false},
-	// 		{ id:4201, pId:42, name:'武汉'},
-	// 		{ id:4302, pId:42, name:'咸宁'},
-	// 	];
-
 //get data by route_uuid
 	if (count($_GET) > 0 && isset($route_uuid)) {
 		$sql = "select * from v_landing_route where route_uuid='$route_uuid' limit 1";
@@ -175,7 +161,7 @@
 				$redis->connect("127.0.0.1", 6379);
 				$redis->auth("8dc40c2c4598ae5a");
 				$redis->select(2);
-				$redis->lpush("php:test:key", time());
+				$redis->lpush("pbx:route:rule:watch", time());
 				unset($redis);
 			}
 
@@ -195,7 +181,7 @@
 				$redis->connect("127.0.0.1", 6379);
 				$redis->auth("8dc40c2c4598ae5a");
 				$redis->select(2);
-				$redis->lpush("php:test:key", time());
+				$redis->lpush("pbx:route:rule:watch", time());
 				unset($redis);
 			}
 
