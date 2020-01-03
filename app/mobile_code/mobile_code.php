0930f0fba4aeb4021f960193251bce9bdca4e26e
hzxgo <hezhixiongbei@126.com>
2020-01-03 18:02:06 +0800

    add mobile code

---
+++ app/mobile_code/mobile_code.php
@@ -0,0 +1,261 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2018
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+
+//check permissions
+	if (permission_exists('mobile_code_view')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
+
+//get the action
+	if (is_array($_POST["data_list"])) {
+		$data_list = $_POST["data_list"];
+		foreach($data_list as $row) {
+			if ($row['action'] == 'delete') {
+				$action = 'delete';
+				$id = $row['mobile_prefix'];
+				break;
+			}
+		}
+		unset($data_list);
+	}
+
+//delete the mobile_prefix
+	if (permission_exists('mobile_code_delete') && ($action == "delete") && (strlen($id) > 0)) {
+		$sql = "delete from v_mobile_code ";
+		$sql .= "where mobile_prefix = '".$id."' ";
+		$db->exec(check_sql($sql));
+		unset($sql, $id);
+
+		//delete message
+		messages::add($text['message-delete']);
+		header("Location: mobile_code.php");
+		return;
+	}
+
+//get variables used to control the order
+	$order_by = check_str($_GET["order_by"]);
+	$order = check_str($_GET["order"]);
+
+//add the search term
+	$search = strtolower(check_str($_GET["search"]));
+	if (strlen($search) > 0) {
+		$sql_search = " (";
+		$sql_search .= "lower(mobile_prefix) like '%".$search."%' ";
+		$sql_search .= "or lower(province) like '%".$search."%' ";
+		$sql_search .= "or lower(city) like '%".$search."%' ";
+		$sql_search .= "or lower(area_code) like '%".$search."%' ";
+		$sql_search .= ") ";
+	}
+
+//additional includes
+	require_once "resources/header.php";
+	require_once "resources/paging.php";
+
+//prepare to page the results
+	$sql = "select count(mobile_prefix) as num_rows from v_mobile_code ";
+	$sql .= "where 1 = 1 ";
+	if (isset($sql_search)) {
+			$sql .= "and ".$sql_search;
+	}
+	if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
+	$prep_statement = $db->prepare($sql);
+	if ($prep_statement) {
+		$prep_statement->execute();
+		$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+		if ($row['num_rows'] > 0) {
+			$num_rows = $row['num_rows'];
+		}
+		else {
+			$num_rows = '0';
+		}
+	}
+
+//prepare to page the results
+	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
+	$param = "&search=".escape($search);
+	if ($_GET['show'] == "all") {
+		$param .= "&show=all";
+	}
+	$page = $_GET['page'];
+	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
+	list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
+	$offset = $rows_per_page * $page;
+
+//get the list
+	$sql = "select * from v_mobile_code ";
+	$sql .= "where 1 = 1 ";
+	if (isset($sql_search)) {
+		$sql .= "and ".$sql_search;
+	}
+	if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
+	$sql .= "limit $rows_per_page offset $offset ";
+	$prep_statement = $db->prepare(check_sql($sql));
+	$prep_statement->execute();
+	$data_list = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+	unset ($prep_statement, $sql);
+
+//alternate the row style
+	$c = 0;
+	$row_style["0"] = "row_style0";
+	$row_style["1"] = "row_style1";
+
+//define the checkbox_toggle function
+	echo "<script type=\"text/javascript\">\n";
+	echo "	function checkbox_toggle(item) {\n";
+	echo "		var inputs = document.getElementsByTagName(\"input\");\n";
+	echo "		for (var i = 0, max = inputs.length; i < max; i++) {\n";
+	echo "		    if (inputs[i].type === 'checkbox') {\n";
+	echo "		       	if (document.getElementById('checkbox_all').checked == true) {\n";
+	echo "				inputs[i].checked = true;\n";
+	echo "			}\n";
+	echo "				else {\n";
+	echo "					inputs[i].checked = false;\n";
+	echo "				}\n";
+	echo "			}\n";
+	echo "		}\n";
+	echo "	}\n";
+	echo "</script>\n";
+
+//show the content
+	echo "<table width='100%' border='0'>\n";
+	echo "	<tr>\n";
+	echo "		<td width='50%' align='left' nowrap='nowrap'><b>".$text['title-mobile_code']."  (".$num_rows.")</b></td>\n";
+	echo "		<form method='get' action=''>\n";
+	echo "			<td width='50%' style='vertical-align: top; text-align: right; white-space: nowrap;'>\n";
+
+	if (permission_exists('mobile_code_all')) {
+		if ($_GET['show'] == 'all') {
+			echo "		<input type='hidden' name='show' value='all'>";
+		}
+		else {
+			echo "		<input type='button' class='btn' value='".$text['button-show_all']."' onclick=\"window.location='mobile_code.php?show=all';\">\n";
+		}
+	}
+
+	echo "				<input type='text' class='txt' style='width: 150px; margin-left: 15px;' name='search' id='search' value='".escape($search)."'>\n";
+	echo "				<input type='submit' class='btn' name='submit' value='".$text['button-search']."'>\n";
+	echo "			</td>\n";
+	echo "		</form>\n";
+	echo "	</tr>\n";
+	echo "	<tr>\n";
+	echo "		<td align='left' colspan='2' valign='top'>\n";
+	echo "			".$text['description-mobile-code']."<br /><br />\n";
+	echo "		</td>\n";
+	echo "	</tr>\n";
+	echo "</table>\n";
+
+	echo "<form method='post' action=''>\n";
+	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+	echo "<tr>\n";
+	echo "	<th style='width:30px;'>\n";
+	echo "		<input type='checkbox' name='checkbox_all' id='checkbox_all' value='' onclick=\"checkbox_toggle();\">\n";
+	echo "	</th>\n";
+
+	echo th_order_by('mobile_prefix', $text['label-mobile_prefix'], $order_by, $order, $param);
+	echo th_order_by('province', $text['label-province'], $order_by, $order, $param);
+	echo th_order_by('city', $text['label-city'], $order_by, $order, $param);
+	echo th_order_by('area_code', $text['label-area_code'], $order_by, $order, $param);
+	echo th_order_by('mobile_isp', $text['label-mobile_isp'], $order_by, $order, $param);
+	echo "	<td class='list_control_icons'>";
+	if (permission_exists('mobile_code_add')) {
+		echo "		<a href='mobile_code_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
+	}
+	else {
+		echo "&nbsp;\n";
+	}
+	echo "	</td>\n";
+	echo "</tr>\n";
+
+	if (is_array($data_list)) {
+		$x = 0;
+		foreach($data_list as $row) {
+			if (permission_exists('mobile_code_edit')) {
+				$tr_link = "href='mobile_code_edit.php?id=".urlencode($row['mobile_prefix'])."'";
+			}
+			echo "<tr ".$tr_link.">\n";
+			echo "	<td valign='top' class='".$row_style[$c]." tr_link_void' style='align: center; padding: 3px 3px 0px 8px;'>\n";
+			echo "		<input type='checkbox' name=\"data_list[$x][checked]\" id='checkbox_".$x."' value='true' onclick=\"if (!this.checked) { document.getElementById('chk_all_".$x."').checked = false; }\">\n";
+			echo "		<input type='hidden' name=\"data_list[$x][mobile_prefix]\" value='".escape($row['mobile_prefix'])."' />\n";
+			echo "	</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['mobile_prefix'])."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['province'])."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['city'])."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['area_code'])."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['mobile_isp'])."&nbsp;</td>\n";
+			echo "	<td class='list_control_icons'>";
+			if (permission_exists('mobile_code_edit')) {
+				echo "<a href='mobile_code_edit.php?id=".escape($row['mobile_prefix'])."' alt='".$text['button-edit']."'>$v_link_label_edit</a>";
+			}
+			if (permission_exists('moblile_code_delete')) {
+				echo "<button type='submit' class='btn btn-default list_control_icon' name=\"data_list[$x][action]\" alt='".$text['button-delete']."' value='delete'><span class='glyphicon glyphicon-remove'></span></button>";
+			}
+			echo "	</td>\n";
+			echo "</tr>\n";
+			$x++;
+			if ($c==0) { $c=1; } else { $c=0; }
+		} //end foreach
+		unset($sql, $data_list, $row_count);
+	} //end if results
+
+	echo "<tr>\n";
+	echo "<td colspan='12' align='left'>\n";
+	echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
+	echo "	<tr>\n";
+	echo "		<td width='33.3%' nowrap='nowrap'>&nbsp;</td>\n";
+	echo "		<td width='33.3%' align='center' nowrap='nowrap'>$paging_controls</td>\n";
+	echo "		<td class='list_control_icons'>";
+	if (permission_exists('mobile_code_add')) {
+		echo 		"<a href='mobile_code_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
+	}
+	else {
+		echo 		"&nbsp;";
+	}
+	echo "		</td>\n";
+	echo "	</tr>\n";
+ 	echo "	</table>\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+	echo "</table>";
+	echo "</form>\n";
+	echo "<br /><br />";
+
+//include the footer
+	require_once "resources/footer.php";
+
+?>
---
+++ app/mobile_code/mobile_code_edit.php
@@ -0,0 +1,269 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2018
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+
+//check permissions
+	if (permission_exists('mobile_code_add') || permission_exists('mobile_code_edit')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
+
+//action add or update
+	if (isset($_REQUEST["id"])) {
+		$action = "update";
+		$mobile_prefix = trim($_REQUEST["id"]);
+	}
+	else {
+		$action = "add";
+	}
+
+//get data by mobile_prefix
+	if (count($_GET) > 0 && isset($mobile_prefix)) {
+		$sql = "select * from v_mobile_code where mobile_prefix='".$mobile_prefix."' limit 1";
+		$prep_statement = $db->prepare(check_sql($sql));
+		$prep_statement->execute();
+		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+		foreach ($result as &$row) {
+			$mobile_prefix = $row["mobile_prefix"];
+			$province = $row["province"];
+			$city = $row["city"];
+			$area_code = $row["area_code"];
+			$mobile_isp = $row["mobile_isp"];
+			break; //limit to 1 row
+		}
+		unset ($prep_statement);
+	}
+
+//get http post variables and set them to php variables
+	if (count($_POST) > 0) {
+
+		//set the variables
+			$mobile_prefix = trim($_POST["mobile_prefix"]);
+			$province = trim($_POST["province"]);
+			$city = trim($_POST["city"]);
+			$area_code = trim($_POST["area_code"]);
+			$mobile_isp = trim($_POST["mobile_isp"]);
+	}
+
+//process the http post 
+	if (count($_POST) > 0 && strlen($_POST["persistformvar"]) == 0) {
+
+		//get the uuid
+			if ($action == "update" && isset($_POST["mobile_prefix"])) {
+				$mobile_prefix = trim($_POST["mobile_prefix"]);
+			}
+			else {
+				$mobile_prefix = trim($_POST["mobile_prefix"]);
+			}
+
+		//check for all required data
+			$msg = '';
+			if (strlen($mobile_prefix) == 0) { $msg .= $text['message-required']." ".$text['label-mobile_prefix']."<br>\n"; }
+			if (strlen($area_code) == 0) { $msg .= $text['message-required']." ".$text['label-area_code']."<br>\n"; }
+
+		//show the message
+			if (strlen($msg) > 0 && strlen($_POST["persistformvar"]) == 0) {
+				require_once "resources/header.php";
+				require_once "resources/persist_form_var.php";
+				echo "<div align='center'>\n";
+				echo "<table><tr><td>\n";
+				echo $msg."<br />";
+				echo "</td></tr></table>\n";
+				persistformvar($_POST);
+				echo "</div>\n";
+				require_once "resources/footer.php";
+				return;
+			}
+
+		// add
+			if ($action == "add") {
+				$sql = "insert into v_mobile_code ";
+				$sql .= "(";
+				$sql .= "mobile_prefix, ";
+				$sql .= "province, ";
+				$sql .= "city, ";
+				$sql .= "area_code, ";
+				$sql .= "mobile_isp ";
+				$sql .= ") ";
+				$sql .= "values ";
+				$sql .= "(";
+				$sql .= "'$mobile_prefix', ";
+				$sql .= "'$province', ";
+				$sql .= "'$city', ";
+				$sql .= "'$area_code', ";
+				$sql .= "'$mobile_isp' ";
+				$sql .= ")";
+				$db->exec(check_sql($sql));
+				unset($sql);
+			}
+
+		// update
+			if ($action == "update" && isset($mobile_prefix)) {
+				
+				$sql = "update v_mobile_code set province='$province', city='$city', ";
+				$sql .= "area_code='$area_code', mobile_isp='$mobile_isp' where mobile_prefix='$mobile_prefix'";
+				$db->exec(check_sql($sql));
+				unset($sql);
+			}
+
+		//redirect the user
+			if ($action == "add") {
+				messages::add($text['message-add']);
+			}
+			if ($action == "update") {
+				messages::add($text['message-update']);
+			}
+			header("Location: mobile_code.php");
+			return;
+
+	} //(count($_POST) > 0 && strlen($_POST["persistformvar"]) == 0)
+
+//show the content
+	echo "<form method='post' name='frm' action=''>\n";
+	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+	echo "<tr>\n";
+	if ($action == "add") {
+		echo "<td align='left' width='30%' nowrap='nowrap' valign='top'><b>".$text['header-mobile-code-add']."</b></td>\n";
+	}
+	if ($action == "update") {
+		echo "<td align='left' width='30%' nowrap='nowrap' valign='top'><b>".$text['header-mobile-code-edit']."</b></td>\n";
+	}
+	echo "<td width='70%' align='right' valign='top'>";
+	echo "	<input type='button' class='btn' alt='".$text['button-back']."' onclick=\"window.location='mobile_code.php'\" value='".$text['button-back']."'>";
+	echo "	<input type='submit' class='btn' value='".$text['button-save']."'>\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "<td align='left' colspan='2'>\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	// mobile_prefix
+	echo "<tr>\n";
+	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-mobile_prefix']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	if ($action == "add") {
+		echo "	<input class='formfld' type='text' name='mobile_prefix' maxlength='255' value=\"".escape($mobile_prefix)."\" required='required'>\n";
+		echo "<br />\n";
+		echo $text['description-mobile_prefix']."\n";
+	}
+	if ($action == "update") {
+		echo "	<input class='formfld' type='text' name='mobile_prefix' maxlength='255' value=\"".escape($mobile_prefix)."\" readonly='readonly'>\n";
+		echo "<br />\n";
+		echo $text['description-mobile_prefix'].$text['description-readonly']."\n";
+	}
+		
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	// province
+	echo "<tr>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-province']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='province' value=\"".escape($province)."\">\n";
+		echo "<br />\n";
+		echo $text['description-province']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	// city
+	echo "<tr>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-city']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='city' value=\"".escape($city)."\">\n";
+		echo "<br />\n";
+		echo $text['description-city']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	// area_code
+	echo "<tr>\n";
+	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-area_code']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='area_code' value=\"".escape($area_code)."\">\n";
+		echo "<br />\n";
+		echo $text['description-area_code']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	// mobile_isp
+	echo "<tr>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-mobile_isp']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<select class='formfld' name='mobile_isp' id='mobile_isp'>\n";
+	switch ($route_type) {
+		case "1" : 	$selected[1] = "selected='selected'";	break;
+		case "2" : 	$selected[2] = "selected='selected'";	break;
+		case "3" : 	$selected[2] = "selected='selected'";	break;
+	}
+	echo "	<option value='1' ".$selected[1].">".$text['label-yi_dong']."</option>\n";
+	echo "	<option value='2' ".$selected[2].">".$text['label-lian_tong']."</option>\n";
+	echo "	<option value='3' ".$selected[3].">".$text['label-dian_xin']."</option>\n";
+	unset($selected);
+	echo "	</select>\n";
+	echo "<br />\n";
+	echo $text['description-mobile_isp']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	echo "	<tr>\n";
+	echo "		<td colspan='2' align='right'>\n";
+	if ($action == "update") {
+		echo "		<input type='hidden' name='mobile_prefix' value='".escape($mobile_prefix)."'>\n";
+	}
+	echo "			<br>";
+	echo "			<input type='submit' class='btn' value='".$text['button-save']."'>\n";
+	echo "		</td>\n";
+	echo "	</tr>";
+	echo "</table>";
+	echo "<br><br>";
+	echo "</form>";
+
+//include the footer
+	require_once "resources/footer.php";
+
+?>
