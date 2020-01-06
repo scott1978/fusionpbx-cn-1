87d08c223aab846975eac1561155845e03fbaa54
hzxgo <hezhixiongbei@126.com>
2020-01-06 19:46:59 +0800

    test

--- app/landing_route/route_edit.php
+++ app/landing_route/route_edit.php
@@ -101,11 +101,11 @@
 			$route_telephone = trim($_POST["route_telephone"]);
 			$route_weekday = trim($_POST["route_weekday"]);
 			$route_start_time = trim($_POST["route_start_time"]);
-			if (strlen($route_start_time) == 0) || (preg_match("[0-2][0-9]:[0-5][0-9]", $route_start_time) == 0) {
+			if ((strlen($route_start_time) == 0) || (preg_match("[0-2][0-9]:[0-5][0-9]", $route_start_time) == 0)) {
 				$route_start_time = "00:00";
 			}
 			$route_end_time = trim($_POST["route_end_time"]);
-			if (strlen($route_end_time) == 0) || (preg_match("[0-2][0-9]:[0-5][0-9]", $route_start_time) == 0) {
+			if ((strlen($route_end_time) == 0) || (preg_match("[0-2][0-9]:[0-5][0-9]", $route_start_time) == 0)) {
 				$route_end_time = "23:59";
 			}
 			$route_enabled = trim($_POST["route_enabled"]);
