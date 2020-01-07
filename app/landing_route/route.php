d77198816b7541450159db3d621442be6578a638
hzxgo <hezhixiongbei@126.com>
2020-01-07 10:58:58 +0800

    test

--- app/landing_route/route.php
+++ app/landing_route/route.php
@@ -26,7 +26,7 @@
 
 //includes
 	require_once "root.php";
-	require_once "resources/redis.php"
+	require_once "resources/redis.php";
 	require_once "resources/require.php";
 	require_once "resources/check_auth.php";
 
