83b17ce59c52d46ffc11107077d2995d220b045d
hzxgo <hezhixiongbei@126.com>
2020-01-31 19:12:10 +0800

    city

--- app/cities/app_config.php
+++ app/cities/app_config.php
@@ -53,9 +53,9 @@
 		$apps[$x]['db'][$y]['table']['parent'] = "";
 		$z=0;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "city_uuid";
-		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'int4';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'int';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'int(10)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "id";
